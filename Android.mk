8dff319c035079ab2fd6a86a3f190b54cefd9f7f
Nathan Prevette <nprevette420@gmail.com>
2016-04-24 08:03:47 -0700

    Fix derp

--- Android.mk
+++ Android.mk
@@ -21,7 +21,7 @@
 
 ifeq ($(BOARD_VENDOR),motorola-qcom)
 ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
-ifneq ($(filter mb886 xt897 xt897c,$(TARGET_DEVICE)),)
+ifeq ($(TARGET_DEVICE),moto_msm8960_jbbl)
 
 LOCAL_PATH := $(call my-dir)
 
