c4e33b9b0f901ecddf364bad18f85342ea1ebcce
Nathan Prevette <nprevette420@gmail.com>
2016-04-24 18:22:22 -0700

    Fixes

--- bliss.mk
+++ bliss.mk
@@ -13,7 +13,6 @@ TARGET_SCREEN_HEIGHT := 1280
 # Media
 PRODUCT_COPY_FILES += \
     device/motorola/moto_msm8960_jbbl/config/media_codecs.xml:system/etc/media_codecs.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml
 
 # Release name
 PRODUCT_NAME := bliss_moto_msm8960_jbbl
